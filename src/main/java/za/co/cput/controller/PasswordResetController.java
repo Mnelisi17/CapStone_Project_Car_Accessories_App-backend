1ef7eb77b191b742bddd6377327dfcadb4984a59
Lennox <lennox.lee1927@gmail.com>
2025-10-19 23:38:23 +0200

    CONTROLLERS

--- src/main/java/za/co/cput/controller/PasswordResetController.java
+++ src/main/java/za/co/cput/controller/PasswordResetController.java
@@ -1,5 +1,9 @@
 package za.co.cput.controller;
-
+/*
+222293985
+Lennox Komane
+group 3F
+ */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/za/co/cput/controller/UserController.java
+++ src/main/java/za/co/cput/controller/UserController.java
@@ -1,5 +1,9 @@
 package za.co.cput.controller;
-
+/*
+222293985
+Lennox Komane
+group 3F
+ */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
